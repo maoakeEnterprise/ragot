5aeebaeae9f6582b1a9dd1a8794a38953c24b580
Maoake <maoake.terii@gmail.com>
2026-09-23 20:51:10 +0200

    need to make the algorithm on each function in the class Tokenizer

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -1,7 +1,23 @@
 from utils.text_utils import STOP_WORDS
+import re
 
 
 class Tokenizer:
     def __init__(self) -> None:
         self.stop_words: set[str] = STOP_WORDS
-        self.word_reg = "[A-Za-z_][A-Za-z0-9_]*"
+        self.word_re = re.compile("[A-Za-z_][A-Za-z0-9_]*")
+        self.camel_re = re.compile(
+            r"(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])"
+            )
+
+    def tokenize(self, text: str) -> list[str]:
+        pass
+
+    def _extract_words(self, text: str) -> list[str]:
+        pass
+
+    def _split_identifier(self, word: str) -> list[str]:
+        pass
+
+    def _is_kept(self, text: str) -> str:
+        pass
