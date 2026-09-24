98a5533a03b5d20cd4f0721796465de6c5c2296d
Maoake <maoake.terii@gmail.com>
2026-09-24 16:16:54 +0200

    fix the tokenizer on some test

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -5,7 +5,6 @@ import re
 class Tokenizer:
     def __init__(self) -> None:
         self.stop_words: set[str] = STOP_WORDS.copy()
-        self.stop_words.add("how")
         self.word_re = re.compile("[A-Za-z_][A-Za-z0-9_]*")
         self.camel_re = re.compile(
             r"(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])"
@@ -13,11 +12,14 @@ class Tokenizer:
 
     def tokenize(self, text: str) -> list[str]:
         words = self._extract_words(text)
-        res: set[str] = set()
+        res: list[str] = []
         for word in words:
             if self._is_kept(word):
-                res.update(self._split_identifier(word))
-                res.add(word)
+                tmp = self._split_identifier(word)
+                if len(tmp) > 1:
+                    res.append(word)
+                tmp = [p for p in tmp if self._is_kept(p)]
+                res.extend(tmp)
         return [p.lower() for p in res]
 
     def _extract_words(self, text: str) -> list[str]:
@@ -28,7 +30,7 @@ class Tokenizer:
         res: list[str] = []
         for x in tab:
             res.extend(self.camel_re.split(x))
-        return [i.lower() for i in res]
+        return [i.lower() for i in res if i != '']
 
     def _is_kept(self, text: str) -> bool:
         if len(text) < 2:
